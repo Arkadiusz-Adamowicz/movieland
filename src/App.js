2203c7e3ff62a1d3df9a8dd25e46874a34c568ea
Arkadiusz Adamowicz <arek.adamowicz81@gmail.com>
2024-05-08 23:05:11 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
 
   useEffect(() => {
     searchMovies(searchTerm);
-  }, []);
+  }, [searchTerm]);
 
   const searchMovies = async title => {
     const res = await fetch(`${API_URL}&s=${title}`);
